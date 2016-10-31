ec76d89cdfed86c7ea10cf03bfc65bdbad9ea28b
srajsu <srajsu@gmail.com>
2016-10-31 20:30:00 +0000

    Application layout
    Designing the application layout

---
+++ 15039352_MobileComputingCW1/15039352_MobileComputingCW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  15039352_MobileComputingCW1
+//
+//  Created by Steven Gatt on 31/10/2016.
+//  Copyright © 2016 Steven Gatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
