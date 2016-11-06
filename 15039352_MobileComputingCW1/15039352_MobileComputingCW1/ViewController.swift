93b8be685849a50aeba8445e4716bf818ca8f5cd
srajsu <srajsu@gmail.com>
2016-11-06 18:39:12 +0100

    Replay option
    Added the replay option upon providing correct answer

--- 15039352_MobileComputingCW1/15039352_MobileComputingCW1/ViewController.swift
+++ 15039352_MobileComputingCW1/15039352_MobileComputingCW1/ViewController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Declaring the Apple images
+    @IBOutlet weak var Apple1: UIImageView!
+    @IBOutlet weak var Apple2: UIImageView!
+    @IBOutlet weak var Apple3: UIImageView!
+    @IBOutlet weak var Apple4: UIImageView!
+    @IBOutlet weak var Apple5: UIImageView!
+    @IBOutlet weak var Apple6: UIImageView!
+    @IBOutlet weak var Apple7: UIImageView!
+    @IBOutlet weak var Apple8: UIImageView!
+    
+    @IBOutlet weak var viewAnswer: UIView!
+    
+    
+    //Declaring the labels which will be used for the addition
+    
+    @IBOutlet weak var lblNoOne: UILabel!
+    @IBOutlet weak var lblNoTwo: UILabel!
+    @IBOutlet weak var lblAns: UILabel!
+    @IBOutlet weak var lblResult: UILabel!
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Generating a random number to be displayed in both labels
+        lblNoOne.text = String(arc4random_uniform(4))
+        lblNoTwo.text = String(arc4random_uniform(4))
+    }
+    
+    
+   
     //Declaring the numbers as buttons
     @IBAction func Number0(_ sender: UIButton) {let userInput = Int(sender.currentTitle!)
         calcAns(userAnswer: userInput!)
     }
+    
     @IBAction func Number1(_ sender: UIButton) {let userInput = Int(sender.currentTitle!)
         calcAns(userAnswer: userInput!)
     }
@@ -42,24 +75,7 @@ class ViewController: UIViewController {
         calcAns(userAnswer: userInput!)
     }
     
-    //Declaring the Apple images
-    @IBOutlet weak var Apple1: UIImageView!
-    @IBOutlet weak var Apple2: UIImageView!
-    @IBOutlet weak var Apple3: UIImageView!
-    @IBOutlet weak var Apple4: UIImageView!
-    @IBOutlet weak var Apple5: UIImageView!
-    @IBOutlet weak var Apple6: UIImageView!
-    @IBOutlet weak var Apple7: UIImageView!
-    @IBOutlet weak var Apple8: UIImageView!
     
-    
-    //Declaring the labels which will be used for the addition
-    
-    @IBOutlet weak var lblNoOne: UILabel!
-    @IBOutlet weak var lblNoTwo: UILabel!
-    @IBOutlet weak var lblAns: UILabel!
-    @IBOutlet weak var lblResult: UILabel!
-        
     @IBAction func calcAns(userAnswer : Int)
     {
         
@@ -71,7 +87,7 @@ class ViewController: UIViewController {
         lblAns.text = String(sum)
         
         if ( Int(sum) == Int(userAnswer)){
-            lblResult.text = "success"
+           self.viewAnswer.isHidden = false
             
         }
         else {
@@ -79,47 +95,9 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //Generating a random number to be displayed in both labels
-        lblNoOne.text = String(arc4random_uniform(4))
-        lblNoTwo.text = String(arc4random_uniform(4))
-    }
+   
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in (touches) {
-            let location = touch.location(in: self.view)
-            
-            if Apple1.frame.contains(location){
-                Apple1.center = location
-            }
-            else if Apple2.frame.contains(location){
-                Apple2.center = location
-            }
-            else if Apple3.frame.contains(location){
-                Apple3.center = location
-            }
-            else if Apple4.frame.contains(location){
-                Apple4.center = location
-            }
-            else if Apple5.frame.contains(location){
-                Apple5.center = location
-            }
-            else if Apple6.frame.contains(location){
-                Apple6.center = location
-            }
-            else if Apple7.frame.contains(location){
-                Apple7.center = location
-            }
-            else if Apple8.frame.contains(location){
-                Apple8.center = location
-            }
-            
-        }
-    }
-    
+       
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in (touches) {
@@ -151,6 +129,24 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+  //  @IBOutlet weak var viewAnswer: UIView!
+    
+ //   @IBAction func btnPlayAgain(_ sender: AnyObject) {
+        
+  //      self.viewAnswer.isHidden = true
+        //self.loadView()
+ //   }
+ 
+  
+    @IBAction func btnPlayAgain(_ sender: UIButton) {
+        self.viewAnswer.isHidden = true
+        self.loadView()
+        viewDidLoad()
+        
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
